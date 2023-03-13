1cec881ac0520e82ca29bbf24f7eaaaed1b7f889
harungokcegoz <harungokcegoz92@gmail.com>
2023-03-13 13:49:24 +0100

    Angela exercises are added

---
+++ Angela/Quizzler-iOS13-master/Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Harun Gökçegöz on 13/03/2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let q: String
+    let a: String
+}
